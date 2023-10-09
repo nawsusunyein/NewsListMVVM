af99eaf228359920e58dcc6a677d47797b779a88
Naw Su Su Nyein <nawsusunyein@gmail.com>
2023-10-09 18:27:05 +0900

    Create project

---
+++ NewsList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsList
+//
+//  Created by Naw Su Su Nyein on 09/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
